86bbcb261a6455de5915efba8a56a5e0c391672d
emmanuel <blessedemmachukwu2020@gmail.com>
2023-06-24 16:28:49 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# App Readme
+
+This is an app created by Chukwu Emmanuel, inspired by the Holy Spirit. The app allows users to publish and display content with endorsements.
+
+## Features
+
+- Publish Content: Users can enter their content, specify the recipient (to), sender (from), and publish it.
+- Endorsements: Users can endorse the published content by liking it.
+- Real-time Updates: The app updates dynamically to reflect changes in the content and endorsements.
+
+## Usage
+
+To use the app, follow these steps:
+
+1. Open the app in a web browser.
+2. Enter the content in the "Content" input field.
+3. Specify the recipient in the "To" input field.
+4. Specify the sender in the "From" input field.
+5. Click the "Publish" button to publish the content.
+6. The published content will appear below in the endorsements section.
+7. To endorse a piece of content, click the heart icon next to it.
+8. The number of likes will be displayed next to the heart icon.
+
+## Dependencies
+
+The app utilizes the following external dependencies:
+
+- `uuid` (version 4): A library for generating UUIDs. The app uses it to assign unique identifiers to each published content.
+
+## Development
+
+If you want to modify or extend the app, follow these steps:
+
+1. Clone the repository.
+2. Open the project in your preferred code editor.
+3. Make the necessary changes to the code.
+4. Test the app locally to ensure it functions as expected.
+5. Deploy the updated app to a web server or hosting platform.
+
+## Credits
+
+- Chukwu Emmanuel: App Author
+
+## License
+
+The app is currently not licensed, and all rights are reserved by the author, Chukwu Emmanuel.
---
+++ index.js
@@ -0,0 +1,86 @@
+import { v4 as uuidv4 } from "https://jspm.dev/uuid";
+
+// const publish = document.getElementById("publish");
+
+const myContent = [
+  {
+    to: "Jane Doe",
+    text: "I love this app",
+    from: "John Doe",
+    isLiked: false,
+    likes: 1,
+    id: "e2fb8758-a373-4981-addc-0b943dcb428a",
+  },
+];
+
+// publish.addEventListener("click", function () {
+//   const contentValue = content.value;
+//   myContent.push(contentValue);
+//   // showContent();
+//   render();
+// });
+
+document.addEventListener("click", function (e) {
+  if (e.target.dataset.like) {
+    handleLikeClick(e.target.dataset.like);
+  } else if (e.target.id === "publish-btn") {
+    handlePulishClick();
+  }
+});
+
+function handlePulishClick() {
+  const contentInput = document.getElementById("content");
+  const toInput = document.getElementById("to");
+  const fromInput = document.getElementById("from");
+  from;
+  if (contentInput.value) {
+    myContent.unshift({
+      to: toInput.value,
+      text: contentInput.value,
+      from: fromInput.value,
+      isLiked: false,
+      likes: 0,
+      id: uuidv4(),
+    });
+  }
+  render();
+  contentInput.value = "";
+  toInput.value = "";
+  fromInput.value = "";
+}
+
+function handleLikeClick(likesId) {
+  const likeObj = myContent.filter(function (content) {
+    return content.id === likesId;
+  })[0];
+
+  likeObj.isLiked ? likeObj.likes-- : likeObj.likes++;
+  likeObj.isLiked = !likeObj.isLiked;
+  render();
+}
+
+function getContent() {
+  let feedHtml = ``;
+
+  myContent.forEach(function (content) {
+    feedHtml += ` <div class="endorsments">
+    <h2 class="h-to">${content.to}</h2>
+    <p class="p-text">${content.text}</p>
+    <h2 class="h-from">${content.from}</h2>
+
+    <span>
+        <i class="fa-solid fa-heart" style="color: #e1630e;" data-like=${content.id}></i>
+    <span>${content.likes}</span>
+    </span>
+
+`;
+  });
+
+  return feedHtml;
+}
+
+function render() {
+  document.getElementById("endorsments").innerHTML = getContent();
+}
+
+render();
